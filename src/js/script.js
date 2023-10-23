617d99b457a91680293d6a8e0710d56ef073b1ec
Egor <iegorka23@gmail.com>
2023-10-23 15:23:53 +0300

    FormsJSONandPHP

--- src/js/script.js
+++ src/js/script.js
@@ -165,10 +165,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // КАРТОЧКИ С МЕНЮ ШАБЛОНИЗИРОВАТЬ И СОЗДАВАТЬ ИХ ТОЛЬКО ПЕРЕДАВАЯ НУЖНЫЕ АРГУМЕНТЫ
 
-    // Используем классны для карточек
+    // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title,descr, price, parentSelector, ...classes) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
@@ -235,6 +235,53 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container'
     ).render();
 
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо! скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    }
+    forms.forEach(item => {
+        postData(item);
+    });
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.succes;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }), 2000;
+                }else {statusMessage.textContent = message.failure}
+            });
+        });
+    }
 });
 
 
